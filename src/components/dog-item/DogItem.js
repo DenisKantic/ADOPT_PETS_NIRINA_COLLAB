cb1aca092120e68aec2c7ea8401ebc84e408265e
DenisKantic <denis.kantic18@gmail.com>
2023-07-07 11:44:32 +0200

    fix for swiper

--- src/components/dog-item/DogItem.js
+++ src/components/dog-item/DogItem.js
@@ -1,10 +1,10 @@
-import dog1 from '../assets/dog1.jpg';
-import dog2 from '../assets/dog2.jpg';
-import dog3 from '../assets/dog3.jpg';
-import dog4 from '../assets/dog4.jpg';
-import dog5 from '../assets/dog5.jpg';
-import dog6 from '../assets/dog6.jpg';
-import dog7 from '../assets/dog7.jpg';
+import dog1 from '../../assets/dog1.jpg';
+import dog2 from '../../assets/dog2.jpg';
+import dog3 from '../../assets/dog3.jpg';
+import dog4 from '../../assets/dog4.jpg';
+import dog5 from '../../assets/dog5.jpg';
+import dog6 from '../../assets/dog6.jpg';
+import dog7 from '../../assets/dog7.jpg';
 
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import React, { useState } from 'react';
