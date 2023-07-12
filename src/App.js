5426fbc4882deafb09a013d2bff6e261778bcdec
DenisKantic <denis.kantic18@gmail.com>
2023-07-12 22:47:30 +0200

    pushing so I can continue later

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import Navigation from "./components/HomePage/Navigation";
 // import Adopt from "./components/Adopt/Adopt";
 import Team from "./components/AboutUs/Team";
+import Donate from "./components/Donate/Donate";
 
 function App() {
   return (
     <div className="App">
-     <Team />
+     <Donate />
     </div>
   );
 }
--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import Navigation from '../HomePage/Navigation'
+import dog_image from '../../images/dog_donation.jpg';
+import {GiOpenedFoodCan} from 'react-icons/gi';
 
 const Donate = () => {
   return (
     <div>
         <Navigation />
-        Donate
+        <div className='w-full relative'>
+        <img src={dog_image} alt="" className='w-full object-cover h-[80vh]' />
+        <div className='absolute top-1/2 left-1/2 -translate-x-4/1 -translate-y-1/2 tracking-wider'>
+                    <h1 className='font-[500] text-white
+                              xss:text-[90px] md:text-[110px]'>DONATE</h1>
+                    <h1 className='text-[50px] text-white bg-gray-500 px-2'>FOR THESE CUTE ANGELS</h1>
+                      </div>
+    </div>
+
+    {/* cards */}
+    <div className='flex justify-center mt-[-150px] relative'>
+      <div className='h-[300px] w-[250px] bg-purple-400 flex flex-col items-center justify-center p-4'>
+        <GiOpenedFoodCan size={70} />
+        <h1>How your donation help us?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, quidem!</p>
+      </div>
+
+      <div className='h-[300px] w-[250px] bg-red-400 flex flex-col items-center justify-center p-4'>
+      <GiOpenedFoodCan size={70} />
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, ut?</p>
+      </div>
+
+      <div className='h-[300px] w-[250px] bg-red-400 flex flex-col items-center justify-center p-4'>
+      <GiOpenedFoodCan size={70} />
+      <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, ut?</p>
+      </div>
+
+      <div className='h-[300px] w-[250px] bg-red-400 flex flex-col items-center justify-center p-4'>
+      <GiOpenedFoodCan size={70} />
+      <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, ut?</p>
+      </div>
+    </div>
+
     </div>
   )
 }
