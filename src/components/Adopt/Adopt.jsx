1d527fe83374677c14feead12a8008b8977ce27c
DenisKantic <denis.kantic18@gmail.com>
2023-08-02 22:16:14 +0200

    fully created adoption list with data.js

--- src/components/Adopt/Adopt.jsx
+++ src/components/Adopt/Adopt.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import { useState } from 'react';
+import {data} from './data';
+
 
 const Adopt = () => {
 
+    const [gender,setGender] = useState("male");
+    const [size, setSize] = useState(["small","medium","large"]);
+
+
     return (
-    <div>
-        Adopt!
+    <div className='mx-auto min-h-[120vh]'>
+        <div className='w-[90%] mx-auto'>
+            <h1 className='text-center text-4xl pt-10'>Take a look at our wonderful dogs!</h1>
+
+            <p className='text-center w-[60%] mx-auto p-5 mb-5'>
+                Scroll through the table below to see all the beautiful dogs that are currently ready for adoption. <br />
+                For a larger view, click “View larger version” in the lower right corner of the table.
+            </p>
+
+            <p className='text-center italic mb-5'>
+            Note: several dogs are still receiving medical treatment and many are in rehabilitation. These dogs are not listed here. 
+            </p>
+           
+           {/* div about filters */}
+            <div className='flex justify-center items-center'> 
+            <p className='mr-10 text-xl'>Sort by size: 
+            <select name="size" id="size" className='ml-2 outline-none bg-slate-300 p-2'>
+                <option value="small">Small</option>
+                <option value="medium">Medium</option>
+                <option value="large">Large</option>
+            </select>
+            </p>
+
+            <p className='text-xl'>Sort by gender: 
+                <select name="" id="" className='ml-2 outline-none bg-slate-300 p-2'>
+                    <option value="">Male</option>
+                    <option value="">Female</option>
+                </select>
+            </p>
+            </div> {/* end of the filter div*/}
+
+            <div className='grid mt-20 mx-auto
+                            xss:grid-cols-1 mdd:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'> {/* dog list */}
+                {data.map(item=>{
+                   return <div className='bg-slate-200 h-[500px] w-[300px] pb-10 mx-auto mt-10 rounded-xl' key={item.id}>
+                    <img className='rounded-t-xl' src={item.pictures[1]} alt="" />
+                        <div className='flex flex-col items-start p-4'>
+                        <h1 className='text-xl'>{item.name}</h1>
+                        <p className='text-xs py-2'>SEX</p>
+                        <p className={gender === item.sex ? 'text-xl bg-green-400 px-3 rounded-xl' : 'text-xl bg-pink-400 px-3 rounded-xl'}>{item.sex}</p>
+                        <p className='text-xs py-2'>SIZE</p>
+                        <p className={size[0] === item.size ? 'text-xl bg-purple-400 px-3 rounded-xl' : 'text-xl bg-slate-500 px-3 rounded-xl'}>{item.size}</p>
+                        <p className='text-sm w-full text-justify p-2'>{item.description}</p>
+                        <p className='p-2'>ID: {item.id}</p>
+                        </div>
+                   </div>
+                })}
+            </div>
+            
+        </div>
     </div>
     )
 }
---
+++ src/components/Adopt/data.js
@@ -0,0 +1,62 @@
+export const data = [
+    {
+        birth: "2021",
+        id: 1001,
+        name: "Dog1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo.",
+        size: "medium",
+        sex: "male",
+        pictures: [
+            "https://scontent.ftzl2-1.fna.fbcdn.net/v/t39.30808-6/361920656_614623024107748_2255681029797164722_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=0debeb&_nc_ohc=qD_1Z0h8AdUAX-nnzD6&_nc_ht=scontent.ftzl2-1.fna&oh=00_AfBNPoa5v-dWkdUHzJrddDxQT1Wo3j93XcYTwn2P3TyRTg&oe=64CFB8D8",
+            "https://scontent.ftzl2-1.fna.fbcdn.net/v/t39.30808-6/361633017_614623190774398_4295789158079889082_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0debeb&_nc_ohc=d4ceGR0gkuQAX_I56OF&_nc_ht=scontent.ftzl2-1.fna&oh=00_AfAXFXqnD0s5mvfq3QA6JUEZm38f4NBdsVolu86si8FXPw&oe=64CFC3BC"
+        ]   
+    },
+    {
+        birth: "2019",
+        name: "Dog1",
+        id: 2121,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo.",
+        size: "big",
+        sex: "female",
+        pictures: [
+            "https://scontent.ftzl2-1.fna.fbcdn.net/v/t39.30808-6/361920656_614623024107748_2255681029797164722_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=0debeb&_nc_ohc=qD_1Z0h8AdUAX-nnzD6&_nc_ht=scontent.ftzl2-1.fna&oh=00_AfBNPoa5v-dWkdUHzJrddDxQT1Wo3j93XcYTwn2P3TyRTg&oe=64CFB8D8",
+            "https://scontent.ftzl2-1.fna.fbcdn.net/v/t39.30808-6/361633017_614623190774398_4295789158079889082_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0debeb&_nc_ohc=d4ceGR0gkuQAX_I56OF&_nc_ht=scontent.ftzl2-1.fna&oh=00_AfAXFXqnD0s5mvfq3QA6JUEZm38f4NBdsVolu86si8FXPw&oe=64CFC3BC"
+        ]   
+    },
+    {
+        birth: "2019",
+        name: "Dog1",
+        id: 20201,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo.",
+        size: "big",
+        sex: "male",
+        pictures: [
+            "https://scontent.ftzl2-1.fna.fbcdn.net/v/t39.30808-6/361920656_614623024107748_2255681029797164722_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=0debeb&_nc_ohc=qD_1Z0h8AdUAX-nnzD6&_nc_ht=scontent.ftzl2-1.fna&oh=00_AfBNPoa5v-dWkdUHzJrddDxQT1Wo3j93XcYTwn2P3TyRTg&oe=64CFB8D8",
+            "https://scontent.ftzl2-1.fna.fbcdn.net/v/t39.30808-6/361633017_614623190774398_4295789158079889082_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0debeb&_nc_ohc=d4ceGR0gkuQAX_I56OF&_nc_ht=scontent.ftzl2-1.fna&oh=00_AfAXFXqnD0s5mvfq3QA6JUEZm38f4NBdsVolu86si8FXPw&oe=64CFC3BC"
+        ]   
+    },
+    {
+        birth: "2019",
+        name: "Dog1",
+        id: 2301,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo.",
+        size: "big",
+        sex: "female",
+        pictures: [
+            "https://scontent.ftzl2-1.fna.fbcdn.net/v/t39.30808-6/361920656_614623024107748_2255681029797164722_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=0debeb&_nc_ohc=qD_1Z0h8AdUAX-nnzD6&_nc_ht=scontent.ftzl2-1.fna&oh=00_AfBNPoa5v-dWkdUHzJrddDxQT1Wo3j93XcYTwn2P3TyRTg&oe=64CFB8D8",
+            "https://scontent.ftzl2-1.fna.fbcdn.net/v/t39.30808-6/361633017_614623190774398_4295789158079889082_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0debeb&_nc_ohc=d4ceGR0gkuQAX_I56OF&_nc_ht=scontent.ftzl2-1.fna&oh=00_AfAXFXqnD0s5mvfq3QA6JUEZm38f4NBdsVolu86si8FXPw&oe=64CFC3BC"
+        ]   
+    },
+    {
+        birth: "2019",
+        name: "Dog1",
+        id: 3001,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, illo.",
+        size: "small",
+        sex: "male",
+        pictures: [
+            "https://scontent.ftzl2-1.fna.fbcdn.net/v/t39.30808-6/361920656_614623024107748_2255681029797164722_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=0debeb&_nc_ohc=qD_1Z0h8AdUAX-nnzD6&_nc_ht=scontent.ftzl2-1.fna&oh=00_AfBNPoa5v-dWkdUHzJrddDxQT1Wo3j93XcYTwn2P3TyRTg&oe=64CFB8D8",
+            "https://scontent.ftzl2-1.fna.fbcdn.net/v/t39.30808-6/361633017_614623190774398_4295789158079889082_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0debeb&_nc_ohc=d4ceGR0gkuQAX_I56OF&_nc_ht=scontent.ftzl2-1.fna&oh=00_AfAXFXqnD0s5mvfq3QA6JUEZm38f4NBdsVolu86si8FXPw&oe=64CFC3BC"
+        ]   
+    }   
+];
