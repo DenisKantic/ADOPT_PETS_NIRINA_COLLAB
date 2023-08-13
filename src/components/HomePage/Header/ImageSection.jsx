deb81b50f8135618bf97027f5fc0d3b9d27b573a
DenisKantic <denis.kantic18@gmail.com>
2023-08-14 00:01:18 +0200

    replaced images from png format to webp and deleted the originals

--- src/components/HomePage/Header/ImageSection.jsx
+++ src/components/HomePage/Header/ImageSection.jsx
@@ -1,9 +1,9 @@
 const Image = () => {
     return (
-        <div className="bg-header bg-center flex justify-center items-center h-[80vh]">
-            <h1 className="text-white font-bold text-center
-                            xss:text-4xl xss:w-[90%] md:w-[70%] mdd:w-[60%] mdd:text-5xl">
-                Welcome to Nirina ORG. A place where you can change your life forever!
+        <div className="bg-header2 bg-center flex justify-start p-5 items-center h-[80vh]">
+            <h1 className="text-black font-bold text-center
+                            xss:text-4xl xss:w-[90%] md:w-[70%] mdd:w-[50%] mdd:text-4xl">
+                Welcome to Nirina Animal Shelter. A place where you can change your life forever!
                 </h1>
         </div>
     )
